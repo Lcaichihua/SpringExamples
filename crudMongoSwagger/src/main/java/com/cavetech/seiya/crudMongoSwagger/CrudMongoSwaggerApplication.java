16a53242ef91eb9ff9d47c27da3837bc303bad89
Larry Caichihua Vergara <lcaichihua@hotmail.com>
2020-09-15 00:00:18 -0500

    crud mongo

---
+++ crudMongoSwagger/src/main/java/com/cavetech/seiya/crudMongoSwagger/CrudMongoSwaggerApplication.java
@@ -0,0 +1,14 @@
+package com.cavetech.seiya.crudMongoSwagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CrudMongoSwaggerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMongoSwaggerApplication.class, args);
+	}
+
+}
